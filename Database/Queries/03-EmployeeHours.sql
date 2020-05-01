686801f7c78adaeab2791cf9164e2f1babc56ca3
Alex Benlolo <57741934+A-Benlolo@users.noreply.github.com>
2020-04-30 21:10:51 -0400

    Update 03-EmployeeHours.sql
    Removed 2 week constraint.

--- Database/Queries/03-EmployeeHours.sql
+++ Database/Queries/03-EmployeeHours.sql
@@ -2,7 +2,4 @@ SELECT CONCAT(fName, " ", lName) AS "Employee", SUM(hours) AS "Hours"
 FROM Person
 JOIN Employee ON Person.ssn=Employee.ssn
 JOIN Hours ON Employee.ssn=Hours.ssn
-WHERE date >= (
-	SELECT DATE_SUB(CURDATE(), INTERVAL 2 WEEK)
-)
 GROUP BY fName, lName;
